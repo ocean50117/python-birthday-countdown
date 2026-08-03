4ad6b8d77b9fa5c2d69d303e780c402d034f7ad8
ocean50117 <142587283+ocean50117@users.noreply.github.com>
2026-08-03 23:04:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime, bday_messages
 # 2. 取得今天的日期
 today = datetime.date.today()
 # 3. 設定下一個生日的日期
-next_birthday = datetime.date(2027, 7, 3)
+next_birthday = datetime.date(2027, mm, dd)
 # 4. 計算下一個生日距離今天的天數
 days_away = (next_birthday - today).days
 if today == next_birthday:
@@ -20,9 +20,9 @@ months = remaining_days // 30
 print(f'I have lived for {years} years and {months} months!')
 
 # 6. 計算距離 80 歲還有多少天，輸入出生年月日
-birth_year = 1994
-birth_month = 7
-birth_day = 3
+birth_year = yyyy
+birth_month = mm
+birth_day = dd
 eighty_bday = datetime.date(birth_year + 80, birth_month, birth_day)
 days_left = (eighty_bday - today).days
 
